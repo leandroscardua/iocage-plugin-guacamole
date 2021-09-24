2d018ed2735c9dbe36d14804178a61322d43cd35
Leandro Scardua <leandroscar@gmail.com>
2021-09-24 12:41:02 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+mysqlroot=$(openssl rand -base64 15)
+guacamole_password=$(openssl rand -base64 15)
+
+pkg install guacamole-server
+pkg install guacamole-client
+pkg install mysql80-server
+pkg install mysql-connector-java
+
+
+echo 'guacd_enable="YES"' >> /etc/rc.conf
+echo 'tomcat9_enable="YES"' >> /etc/rc.conf
+echo 'mysql_enable="YES"' >> /etc/rc.conf
+
+mkdir /usr/local/etc/guacamole-client/lib
+mkdir /usr/local/etc/guacamole-client/extensions
+
+cp /usr/local/share/java/classes/mysql-connector-java.jar /usr/local/etc/guacamole-client/lib
+
+tar xvfz /usr/local/share/guacamole-client/guacamole-auth-jdbc.tar.gz -C /tmp/
+
+cp /tmp/guacamole-auth-jdbc-*/mysql/*.jar /usr/local/etc/guacamole-client/extensions
+
+cp /usr/local/etc/guacamole-server/guacd.conf.sample /usr/local/etc/guacamole-server/guacd.conf
+
+cp /usr/local/etc/guacamole-client/logback.xml.sample /usr/local/etc/guacamole-client/logback.xml
+
+cp /usr/local/etc/guacamole-client/guacamole.properties.sample /usr/local/etc/guacamole-client/guacamole.properties
+
+
+service mysql-server start
+
+mysql -u root
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$mysqlroot';
+CREATE DATABASE guacamole_db;
+CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY '$guacamole_password';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+FLUSH PRIVILEGES;
+quit
+ls
+
+cat /tmp/guacamole-auth-jdbc-*/mysql/schema/*.sql | mysql -u root -p$mysqlroot guacamole_db
+
+
+service mysql-server restart
+service guacd restart
+service tomcat9 restart
